3c26a968a8edd50915e3138460fcdcf078b55e36
Aaron McCarthy <amccarthy9904@gmail.com>
2017-09-05 21:05:07 -0600

    made seperate case for .data and .name files

--- ARRF_conv.py
+++ ARRF_conv.py
@@ -9,10 +9,19 @@ import re
 
 if __name__ == "__main__":
     for file_name in sys.argv:
-        convert(file_name)
-        
-def convert (file_name):
+        if re.search(file_name, ".*\.name"): 
+            convertName(file_name)
+        elif re.match(file_name, ".*\.data"):
+            convertData(file_name)
+            
+def convertName (file_name):
     with open(file_name, "r") as file:
         for line in file:
-            
+            if re.match(line, '7. Attribute Information'):
+                
+def convertData (file_name):
+    with open(file_name, "r") as file:
+        for line in file:
+             one_line_data = re.split(line, ',,')
+                
     
