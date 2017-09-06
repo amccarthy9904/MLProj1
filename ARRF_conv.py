650a63eb54270b7827f4fa3e19b61473c6eecbc7
Aaron McCarthy <amccarthy9904@gmail.com>
2017-09-05 20:27:12 -0600

    did something last night

--- ARRF_conv.py
+++ ARRF_conv.py
@@ -2,22 +2,25 @@
 """
 Created on Tue Sep  5 20:13:53 2017
 
-@author: oopl
+@author: Aaron McCarthy
+@author: Elias Athey
+@author: Tia Smith
 """
 import sys
 import re
 
 if __name__ == "__main__":
     for file_name in sys.argv:
-        if re.search(file_name, ".*\.name"): 
+        if re.search(file_name, "\.name"): 
             convertName(file_name)
-        elif re.match(file_name, ".*\.data"):
+        elif re.match(file_name, "\.data"):
             convertData(file_name)
             
 def convertName (file_name):
     with open(file_name, "r") as file:
         for line in file:
-            if re.match(line, '7. Attribute Information'):
+            if re.match(line, '7. Attribute Information:'):
+                getAttributes()
                 
 def convertData (file_name):
     with open(file_name, "r") as file:
