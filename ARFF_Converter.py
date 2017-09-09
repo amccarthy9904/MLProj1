8bdc129bcdf420bfd5e5cb4d54a3701536398a9f
Elias Athey <athey.elias@gmail.com>
2017-09-09 15:25:22 -0600

    Finished writing getAttributes method. Added global variable classColumnIndex defining which column the classes are in.

--- ARFF_Converter.py
+++ ARFF_Converter.py
@@ -10,19 +10,49 @@ import sys
 import re
 import os
 
-# TODO
 # Prompts the user for the attributes and associated data types
 # Returns a list of 2-tuples, each corresonding to an attribute name and datatype pair
 def getAttributes(classes):
-	global dataFile
-	# grab the first datapoint from dataFile
-	# remove the class name from the list of values
-	# The sample point should print this >> ClassName: datavalue1 datavalue2 ...
+	global dataFile, classColumnIndex
+	# Iris-Setosa, Iris-Versicolour, Iris-Virginica 
+	# sepal length:NUMERIC, sepal width:NUMERIC, petal length:NUMERIC, petal width:NUMERIC
 	
-	#with open(dataFile, "r") as file:
+	with open(dataFile, 'r') as file:
 	
-	# Return a list of 2-tuples
-	return [("attributeName1", "NUMERIC"), ("attributeName2", "{ Nominal1, Nominal2 }")]
+		# Get the first data point as a list of strings
+		firstLine = file.readline().strip()
+		sampleDataPoint = []
+		
+		# split data on commas or spaces
+		if (re.search(',', firstLine)):
+			sampleDataPoint = firstLine.split(',')
+		else:
+			sampleDataPoint = re.split('\s+', firstLine)
+		sampleDataPoint = [attr.strip().lower() for attr in sampleDataPoint]
+		
+		# Finds the column in which the class is defined
+		# Order of checking: last index, index 0, then index 1
+		# This does not handle every case - open to other suggestions
+		classColumnIndex = -1
+		if (sampleDataPoint[len(sampleDataPoint) - 1] in classes):
+			classColumnIndex = len(sampleDataPoint) - 1
+		elif(sampleDataPoint[0] in classes):
+			classColumnIndex = 0
+		else:
+			classColumnIndex = 1
+		sampleClass = sampleDataPoint[classColumnIndex]
+		sampleDataPoint.pop(classColumnIndex)
+		sampleAttributes = re.sub('\[|\]|\'', '', str(sampleDataPoint))
+		
+		# Message to user and sample data point
+		print("\nThank you.\nBelow is a sample data point.\nPlease enter the attribute names and associated data types in the order they appear in the sample.\nPlease follow this format: <attributeName1>:<dataType1>, <attributeName2>:<dataType2>, ...etc\n<dataType> can be one of NUMERIC, STRING, DATE, or a nominal value.\nIf an attribute has a nominal type, format <dataType> as {nominalValue1, nominalValue2,...,nominalValueN}\n")
+		print("Here is the sample data point of the class", sampleClass + ".")
+		print(sampleAttributes)
+		
+		# return user input as a list of tuples
+		userInput = input()
+		return [attrTuple.strip().split(':') for attrTuple in userInput.split(',')]
+		
 
 # Prompts the user for the data classes
 # Returns a list of the class names
@@ -30,14 +60,14 @@ def getClasses():
 	print("Please enter the class names separated by commas:")
 	classes = input()
 	classes = classes.split(',')
-	classes = [c.strip() for c in classes]
+	classes = [c.strip().lower() for c in classes]
 	return classes
 
 # TODO
 # Given the classes and attributes (with datatypes)
 # Create and format an .arff file in the current directory
 def createARFF(classes, attributes):
-	global dataFile
+	global dataFile, classColumnIndex
 	#with open(dataFile, "r") as file:
 		#write @RELATION tag
 		#write @ATTRIBUTE tags in the order the list is in
@@ -49,7 +79,7 @@ def createARFF(classes, attributes):
 # Given the classes and attributes
 # Format the @DATA section and return it as a string
 def formatData(classes, attributes):
-	global dataFile
+	global dataFile, classColumnIndex
 	# Maybe switch stmt on file type and convert to ARFF format accordingly
 	return "This will be a string of the data section in ARFF: \n @DATA \n atrbt1, atrbt2,...,class \n"
 	
